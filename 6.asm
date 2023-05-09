35686ddc4e6df0750f566871c347c6c6d82848e7
twrkkk <zhenya.shein.03@gmail.com>
2023-05-09 18:42:38 +0300

    f

--- 6.asm
+++ 6.asm
@@ -7,6 +7,7 @@ A dw -1, -3, 5, 7, -9, -2
 c dw -50
 d dw 10
 cdivd dw 0
+outline db "result: ", 0dh, 0ah,'$'
 prod dw 1
 
 .code
@@ -28,8 +29,27 @@ M1:
     mul prod
     mov prod , ax
 M2: 
-    add si,2
-    loop M1
-    
+    lodsw
+    loop M1   
+;print prod    
+    mov ah, 9
+    mov dx, offset outline
+    int 21h    
+    mov ax, prod
+    mov cx, 0
+    mov bx, 10
+mk3: 
+    cwd
+    div bx
+    add dl, '0'
+    push dx
+    inc cx
+    cmp ax, 0
+    jne mk3
+    mov ah, 2
+ol:
+    pop dx
+    int 21h
+    loop ol  
 
 ret
